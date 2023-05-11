0a8fab9a8d8340c0d5624dec70d0a89a2ea1bdf1
Cole Jensen <cole.jensen@yale.edu>
2023-05-11 14:58:34 -0400

    added the new RF distance function

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,8 @@
 ##' @importFrom utils packageDescription
 .onAttach <- function(libname, pkgname) {
-  dowser_ref <- paste("Hoehn K, Pybus O, Kleinstein S (2022). “Phylogenetic analysis of migration, differentiation, and class switching in B cells.” PLoS Computational Biology. https://doi.org/10.1371/journal.pcbi.1009885.")
+  dowser_ref <- paste('Hoehn K, Pybus O, Kleinstein S (2022). Phylogenetic',
+                      'analysis of migration, differentiation, and class switching in B cells. PLoS',
+                      'Computational Biology. https://doi.org/10.1371/journal.pcbi.1009885.')
   ref <- paste0("If you are using dowser in published research please cite ", dowser_ref)
   if(!is.null(ref)) packageStartupMessage(ref)
 }
